6292bb35378567106b163473dbd6f6cb29aeb5c0
javier <javiercarballo2000@gmail.com>
2023-03-31 11:02:51 +0200

    added C.c

---
+++ C.c
@@ -0,0 +1,16 @@
+#include <math.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <inttypes.h>
+#include <omp.h>
+#include <time.h>
+
+void main(int argc, char **argv)
+{
+  printf("argc = %d, argv = %d\n",argc,atoi(argv[1]));
+
+  
+}
